b180e57b679d18aee4abaa2f0089100b33042f67
bali.appkey <bali.appkey@gmail.com>
2020-08-10 13:48:42 +0800

    Try counter from documentation

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div style="text-align: center">
+    <button wire:click="increment"> + </button>
+        <h1>{{ $count }}</h1>
+    <button wire:click="decrement"> - </button>
 </div>
